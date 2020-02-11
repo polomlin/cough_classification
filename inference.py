4843e8d82e2a9ca95817c98ff1de619f0905a042
polomlin <polomlin@gmail.com>
2020-02-11 15:54:18 +0800

    initial commit

---
+++ inference.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from keras.models import Model,load_model
+import os
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import numpy as np
+import magic
+import extract_features
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'wav', 'wave'}
+ALLOWED_MIME_TYPES = {'audio/wav', 'audio/x-wav'}
+
+app = Flask(__name__)
+#app.config["DEBUG"] = True
+app.config["JSON_AS_ASCII"] = False
+
+model_file = 'trained_cnn.h5'
+
+model = load_model(model_file)
+
+def is_allowed_file(file):
+    if '.' in file.filename:
+        ext = file.filename.rsplit('.', 1)[1].lower()
+        print("ext: ", ext)
+    else:
+        return False
+        
+    mime_type = magic.from_buffer(file.stream.read(), mime=True)
+    print('mime_type: ', mime_type)
+    if (
+        mime_type in ALLOWED_MIME_TYPES and
+        ext in ALLOWED_EXTENSIONS
+    ):
+        return True
+        
+    return False 
+
+@app.route('/cough/inference', methods=['POST'])
+def inference():
+    try:
+        f = request.files['file']
+        f.save(os.path.join('/tmp/ramdisk', f.filename))
+        if os.path.exists(os.path.join('/tmp/ramdisk', f.filename)) and os.path.getsize(os.path.join('/tmp/ramdisk', f.filename)) > 0:
+            prediction_feature = extract_features.get_features(os.path.join('/tmp/ramdisk', f.filename))
+            prediction_feature = np.expand_dims(np.array([prediction_feature]),axis=2)
+            predicted_vector = model.predict_classes(prediction_feature)
+            if list(predicted_vector)[0] == 1:
+                return '{"response":{"cough": True}}'
+            else:
+                return '{"response":{"cough": False}}'
+        else:
+            return '{"response":{"cough": False}}'
+    except Exception as e:
+        print('Error: ', e)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=False, threaded = False)         
---
+++ prediction.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from keras.utils import to_categorical
+from keras.models import load_model
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import extract_features
+import nn
+import glob
+import numpy as np
+
+
+def create_cnn(num_labels):
+
+    model = Sequential()
+    model.add(Conv1D(64, 3, activation='relu', input_shape=(40, 1)))
+    model.add(Conv1D(64, 3, activation='relu'))
+    model.add(MaxPooling1D(3))
+    model.add(Conv1D(128, 3, activation='relu'))
+    model.add(Conv1D(128, 3, activation='relu'))
+    model.add(GlobalAveragePooling1D())
+    model.add(Dropout(0.5))
+    model.add(Dense(num_labels))
+    model.add(Activation('softmax'))
+    return model
+
+def predict(filenames,le,model_file, kind):
+    model = load_model(model_file)
+    for filename in filenames:
+        prediction_feature = extract_features.get_features(filename)
+        #if model_file == "trained_mlp.h5":
+        #    prediction_feature = np.array([prediction_feature])
+        #elif model_file == "trained_cnn.h5":
+        prediction_feature = np.expand_dims(np.array([prediction_feature]),axis=2)
+
+        predicted_vector = model.predict_classes(prediction_feature)
+        predicted_proba_vector = model.predict_proba([prediction_feature])
+
+        predicted_proba = predicted_proba_vector[0]
+        for i in range(len(predicted_proba)):
+            category = le.inverse_transform(np.array([i]))
+            print(category[0], "\t\t : ", format(predicted_proba[i], '.32f') )
+
+
+if __name__ == "__main__":
+    le = LabelEncoder()
+    # one hot encoded labels
+    allpaths = glob.glob('/home/sam/Music/cough/asr/test/yes/*.wav')
+    predict(allpaths,le,"trained_cnn.h5",'yes')
+
+    allpaths = glob.glob('/home/sam/Music/cough/asr/test/no/*.wav')
+    predict(allpaths,le,"trained_cnn.h5", 'no')
+
+
